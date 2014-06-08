06ba880ef7650fb9d97f0e38b22d8d0afbc3ae8e
ChupeiZhang <chupeizhang@Chupeis-MacBook-Pro.local>
2014-06-08 02:43:15 -0700

    plot 4 change
    repeat in plot, delete the repeated part

--- plot4.R
+++ plot4.R
@@ -7,24 +7,6 @@ power[,1]<-as.Date(power[,1],format="%d/%m/%Y")
 dates <- power[,1]
 times <- power[,2]
 datetime <- strptime(paste(dates,times),format="%Y-%m-%d %H:%M:%S")
-setwd("~/Documents/my classes/Coursera/exploratory data analysis/project 1")
-powerdata <- read.table("household_power_consumption.txt",sep=";",as.is=TRUE,header=TRUE)
-power1 <- subset(powerdata,powerdata$Date=="1/2/2007")
-power2 <- subset(powerdata,powerdata$Date=="2/2/2007")
-power <- rbind(power1,power2)
-power[,1]<-as.Date(power[,1],format="%d/%m/%Y")
-dates <- power[,1]
-times <- power[,2]
-datetime <- strptime(paste(dates,times),format="%Y-%m-%d %H:%M:%S")
-setwd("~/Documents/my classes/Coursera/exploratory data analysis/project 1")
-powerdata <- read.table("household_power_consumption.txt",sep=";",as.is=TRUE,header=TRUE)
-power1 <- subset(powerdata,powerdata$Date=="1/2/2007")
-power2 <- subset(powerdata,powerdata$Date=="2/2/2007")
-power <- rbind(power1,power2)
-power[,1]<-as.Date(power[,1],format="%d/%m/%Y")
-dates <- power[,1]
-times <- power[,2]
-datetime <- strptime(paste(dates,times),format="%Y-%m-%d %H:%M:%S")
 powernew <- cbind(datetime,power)
 png(file="plot4.png",width=480,height=480)
 par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(0,0,2,0))
